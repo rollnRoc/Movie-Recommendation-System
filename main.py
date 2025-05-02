474632f70d8f3af8ad78c5d89d671435c57a8afb
rollnRoc <yuemyi@gmail.com>
2025-05-02 17:01:37 +0300

    first commit

--- main.py
+++ main.py
@@ -8,34 +8,65 @@ import json
 from bs4 import BeautifulSoup
 import pickle
 import requests
+import os
 
-# load the nlp model and tfidf vectorizer from disk
-filename = 'nlp_model.pkl'
-clf = pickle.load(open(filename, 'rb'))
-vectorizer = pickle.load(open('tranform.pkl','rb'))
+# Check if model and vectorizer files exist
+def load_models():
+    try:
+        # Check if files exist first
+        if not os.path.exists('nlp_model.pkl') or not os.path.exists('tranform.pkl'):
+            print("WARNING: Model files not found! Reviews sentiment analysis will not work.")
+            return None, None
+        
+        # Load the nlp model and tfidf vectorizer from disk
+        clf = pickle.load(open('nlp_model.pkl', 'rb'))
+        vectorizer = pickle.load(open('tranform.pkl', 'rb'))
+        
+        # Test vectorizer to make sure it's fitted
+        vectorizer.transform(["test sentence"])
+        
+        return clf, vectorizer
+    except Exception as e:
+        print(f"Error loading models: {str(e)}")
+        print("Reviews sentiment analysis will not work.")
+        return None, None
+
+# Load models at startup
+clf, vectorizer = load_models()
 
 def create_similarity():
-    data = pd.read_csv('main_data.csv')
-    # creating a count matrix
-    cv = CountVectorizer()
-    count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
-    similarity = cosine_similarity(count_matrix)
-    return data,similarity
+    try:
+        data = pd.read_csv('main_data.csv')
+        # creating a count matrix
+        cv = CountVectorizer()
+        count_matrix = cv.fit_transform(data['comb'])
+        # creating a similarity score matrix
+        similarity = cosine_similarity(count_matrix)
+        return data, similarity
+    except Exception as e:
+        print(f"Error creating similarity matrix: {str(e)}")
+        return None, None
+
+# Global variables to store data and similarity matrix
+data = None
+similarity = None
 
 def rcmd(m):
+    global data, similarity
+    
     m = m.lower()
-    try:
-        data.head()
-        similarity.shape
-    except:
+    # Load data and similarity matrix if not already loaded
+    if data is None or similarity is None:
         data, similarity = create_similarity()
+        if data is None or similarity is None:
+            return 'Error loading movie data. Please check if main_data.csv exists.'
+    
     if m not in data['movie_title'].unique():
         return('Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies')
     else:
         i = data.loc[data['movie_title']==m].index[0]
         lst = list(enumerate(similarity[i]))
-        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+        lst = sorted(lst, key = lambda x:x[1], reverse=True)
         lst = lst[1:11] # excluding first item since it is the requested movie itself
         l = []
         for i in range(len(lst)):
@@ -51,8 +82,12 @@ def convert_to_list(my_list):
     return my_list
 
 def get_suggestions():
-    data = pd.read_csv('main_data.csv')
-    return list(data['movie_title'].str.capitalize())
+    try:
+        data = pd.read_csv('main_data.csv')
+        return list(data['movie_title'].str.capitalize())
+    except Exception as e:
+        print(f"Error loading suggestions: {str(e)}")
+        return []
 
 app = Flask(__name__)
 
@@ -60,19 +95,19 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     suggestions = get_suggestions()
-    return render_template('home.html',suggestions=suggestions)
+    return render_template('home.html', suggestions=suggestions)
 
-@app.route("/similarity",methods=["POST"])
+@app.route("/similarity", methods=["POST"])
 def similarity():
     movie = request.form['name']
     rc = rcmd(movie)
-    if type(rc)==type('string'):
+    if type(rc) == type('string'):
         return rc
     else:
-        m_str="---".join(rc)
+        m_str = "---".join(rc)
         return m_str
 
-@app.route("/recommend",methods=["POST"])
+@app.route("/recommend", methods=["POST"])
 def recommend():
     # getting data from AJAX request
     title = request.form['title']
@@ -99,62 +134,81 @@ def recommend():
     suggestions = get_suggestions()
 
     # call the convert_to_list function for every string that needs to be converted to list
-    rec_movies = convert_to_list(rec_movies)
-    rec_posters = convert_to_list(rec_posters)
-    cast_names = convert_to_list(cast_names)
-    cast_chars = convert_to_list(cast_chars)
-    cast_profiles = convert_to_list(cast_profiles)
-    cast_bdays = convert_to_list(cast_bdays)
-    cast_bios = convert_to_list(cast_bios)
-    cast_places = convert_to_list(cast_places)
-    
-    # convert string to list (eg. "[1,2,3]" to [1,2,3])
-    cast_ids = cast_ids.split(',')
-    cast_ids[0] = cast_ids[0].replace("[","")
-    cast_ids[-1] = cast_ids[-1].replace("]","")
-    
-    # rendering the string to python string
-    for i in range(len(cast_bios)):
-        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
-    
-    # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
-    movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
-    
-    casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
-
-    cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
-    print(f"calling imdb api: {'https://www.imdb.com/title/{}/reviews/?ref_=tt_ov_rt'.format(imdb_id)}")
+    try:
+        rec_movies = convert_to_list(rec_movies)
+        rec_posters = convert_to_list(rec_posters)
+        cast_names = convert_to_list(cast_names)
+        cast_chars = convert_to_list(cast_chars)
+        cast_profiles = convert_to_list(cast_profiles)
+        cast_bdays = convert_to_list(cast_bdays)
+        cast_bios = convert_to_list(cast_bios)
+        cast_places = convert_to_list(cast_places)
+        
+        # convert string to list (eg. "[1,2,3]" to [1,2,3])
+        cast_ids = cast_ids.split(',')
+        cast_ids[0] = cast_ids[0].replace("[","")
+        cast_ids[-1] = cast_ids[-1].replace("]","")
+        
+        # rendering the string to python string
+        for i in range(len(cast_bios)):
+            cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
+        
+        # combining multiple lists as a dictionary which can be passed to the html file
+        movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
+        casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
+        cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
+    except Exception as e:
+        print(f"Error processing data: {str(e)}")
+        return render_template('error.html', error="Error processing movie data")
+        
     # web scraping to get user reviews from IMDB site
-    url = f'https://www.imdb.com/title/{imdb_id}/reviews/?ref_=tt_ov_rt'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
+    movie_reviews = {}
+    try:
+        print(f"Calling IMDB API: https://www.imdb.com/title/{imdb_id}/reviews/?ref_=tt_ov_rt")
+        url = f'https://www.imdb.com/title/{imdb_id}/reviews/?ref_=tt_ov_rt'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
 
-    response = requests.get(url, headers=headers)
-    print(response.status_code)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'lxml')
-        soup_result = soup.find_all("div", {"class": "ipc-html-content-inner-div"})
-        print(soup_result)
+        response = requests.get(url, headers=headers)
+        print(f"Response status code: {response.status_code}")
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'lxml')
+            soup_result = soup.find_all("div", {"class": "ipc-html-content-inner-div"})
+            print(f"Found {len(soup_result)} reviews")
 
-        reviews_list = [] # list of reviews
-        reviews_status = [] # list of comments (good or bad)
-        for reviews in soup_result:
-            if reviews.string:
-                reviews_list.append(reviews.string)
-                # passing the review to our model
-                movie_review_list = np.array([reviews.string])
-                movie_vector = vectorizer.transform(movie_review_list)
-                pred = clf.predict(movie_vector)
-                reviews_status.append('Good' if pred else 'Bad')
+            reviews_list = []  # list of reviews
+            reviews_status = []  # list of comments (good or bad)
+            
+            for reviews in soup_result:
+                if reviews.string:
+                    reviews_list.append(reviews.string)
+                    
+                    # Only predict sentiment if models are loaded
+                    if clf is not None and vectorizer is not None:
+                        try:
+                            # passing the review to our model
+                            movie_review_list = np.array([reviews.string])
+                            movie_vector = vectorizer.transform(movie_review_list)
+                            pred = clf.predict(movie_vector)
+                            reviews_status.append('Good' if pred[0] else 'Bad')
+                        except Exception as e:
+                            print(f"Error predicting sentiment: {str(e)}")
+                            reviews_status.append('Unknown')
+                    else:
+                        reviews_status.append('Unknown')
 
-        # combining reviews and comments into a dictionary
-        movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}     
+            # combining reviews and comments into a dictionary
+            movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}
+        else:
+            print(f"Failed to retrieve reviews: {response.status_code}")
+    except Exception as e:
+        print(f"Error retrieving reviews: {str(e)}")
+        # Continue without reviews if there's an error
 
-        # passing all the data to the html file
-        return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
-            vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
-            movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
-    else:
-        print("Failed to retrieve reviews")
+    # passing all the data to the html file
+    return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
+        vote_count=vote_count, release_date=release_date, runtime=runtime, status=status, genres=genres,
+        movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '30a4a2951bbd5f0f67a5ace6cd3c9bac';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '30a4a2951bbd5f0f67a5ace6cd3c9bac';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
